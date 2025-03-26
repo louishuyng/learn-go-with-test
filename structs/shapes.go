5e33449b38e50a227bd4f8570e89d969a3337479
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-03-27 00:01:37 +0700

    Apparently works-for-me is a crappy excuse.

---
+++ structs/shapes.go
@@ -0,0 +1,18 @@
+package structs
+
+type Rectangle struct {
+	width  float64
+	height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.width + r.height)
+}
+
+func Area(r Rectangle) float64 {
+	return r.width * r.height
+}
