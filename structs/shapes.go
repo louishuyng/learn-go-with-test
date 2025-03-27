32e8e47e48b07222e4b1b560fcd9e38b844a607d
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-03-27 13:46:20 +0700

    One day I'll actually start looking at what Copilot generates instead of just immediately pushing it.

--- structs/shapes.go
+++ structs/shapes.go
@@ -1,5 +1,11 @@
 package structs
 
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	width  float64
 	height float64
@@ -9,10 +15,23 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 func Perimeter(r Rectangle) float64 {
 	return 2 * (r.width + r.height)
 }
 
-func Area(r Rectangle) float64 {
+func (r Rectangle) Area() float64 {
 	return r.width * r.height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
