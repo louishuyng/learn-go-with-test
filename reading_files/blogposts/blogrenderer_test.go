644165ec644cd63e1cec67f55a1307f59ca60998
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-03-30 15:40:26 +0700

    minor changes

--- reading_files/blogposts/blogrenderer_test.go
+++ reading_files/blogposts/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogpost
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	approvals "github.com/approvals/go-approval-tests"
@@ -17,13 +18,41 @@ func TestRender(t *testing.T) {
 		}
 	)
 
+	postRenderer, err := NewPostRenderer()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
 
-		if err := Render(&buf, aPost); err != nil {
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
 		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	var (
+		aPost = Post{
+			Title:       "hello world",
+			Body:        "This is a post",
+			Description: "This is a description",
+			Tags:        []string{"go", "tdd"},
+		}
+	)
+
+	postRenderer, err := NewPostRenderer()
+
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
