21a634087a63e0780ad601b12cd8b166891c8198
Louis Huy Nguyen <huynguyennbk@gmail.com>
2025-04-02 21:14:07 +0700

    ajhenriques sucks

--- http-server/main.go
+++ http-server/main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
-type InMemoryPlayerStore struct {
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
+const dbFileName = "game.db.json"
 
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
 
-	log.Fatal(http.ListenAndServe(":5001", server))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	store := NewFileSystemPlayerStore(db)
+
+	server := NewPlayerServer(store)
+
+	if err := http.ListenAndServe(":5001", server); err != nil {
+		log.Fatalf("could not listen on port 5001 %v", err)
+	}
 }
